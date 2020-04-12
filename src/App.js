7377f808682426e736b21a052b77510c93477e52
unknown <m_fattih@hotmail.com>
2020-04-11 20:10:44 -0700

    mvp1

--- src/App.js
+++ src/App.js
@@ -1,61 +1,45 @@
-import React from 'react';
+import React from "react";
 
-import Header from './components/Header';
-import AddedFeatures from './components/AddedFeatures';
-import AdditionalFeatures from './components/AdditionalFeatures';
-import Total from './components/Total';
+import Header from "./components/Header";
+import AddedFeatures from "./components/AddedFeatures";
+import AdditionalFeatures from "./components/AdditionalFeatures";
+import Total from "./components/Total";
 
-import {connect} from "react-redux"
+import { connect } from "react-redux";
 
-import {addFeature,removeFeature,removeFeatureBack,addFeatureBack} from "./actions"
-
+import {
+  addFeature,
+  removeFeature,
+  removeFeatureBack,
+  addFeatureBack,
+} from "./actions";
 
 const App = (props) => {
- 
   //console.log("carFeaturesData Check",  sortObj(props.carFeaturesData))
 
+  function sortObj(recObj) {
+    return {
+      ...recObj,
+      additionalFeatures: recObj.additionalFeatures.sort(function (a, b) {
+        return a.id - b.id;
+      }),
+    };
+  }
 
-    function sortObj (recObj) {
-      return{
-
-        ...recObj,
-        additionalFeatures: recObj.additionalFeatures.sort(
-          function (a, b) {
-            return a.id - b.id;
-          })
-         
-       
-        
-        
-        
-        
-        
-        
-        
-      }
-      }
-      
-
-
-   
-
-  const removeFeature = item => {
+  const removeFeature = (item) => {
     // dispatch an action here to remove an item
-   // console.log("Remove Feature", item)
-   props.removeFeature (item)
-   props.addFeatureBack(item)
-   props.removeFeatureBack(item)
-    
+    // console.log("Remove Feature", item)
+    props.removeFeature(item);
+    props.addFeatureBack(item);
+    props.removeFeatureBack(item);
   };
 
-  const buyItem = item => {
-    
-   // console.log("ADD Feature", item)
-    props.addFeature(item)
-    props.removeFeature (item)
-
+  const buyItem = (item) => {
+    // console.log("ADD Feature", item)
+    props.addFeature(item);
+    props.removeFeature(item);
   };
- 
+
   return (
     <div className="boxes">
       <div className="box">
@@ -63,26 +47,31 @@ const App = (props) => {
         <AddedFeatures removeFeature={removeFeature} car={props.carData.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures buyItem={buyItem} additionalFeatures={  sortObj(props.carFeaturesData).additionalFeatures} />
-        <Total car={props.carData.car} additionalPrice={props.carFeaturesData.additionalPrice} />
+        <AdditionalFeatures
+          buyItem={buyItem}
+          additionalFeatures={sortObj(props.carFeaturesData).additionalFeatures}
+        />
+        <Total
+          car={props.carData.car}
+          additionalPrice={props.carFeaturesData.additionalPrice}
+        />
       </div>
     </div>
   );
-   
 };
 
 const mapStateToProps = (globalState) => {
-return{
-  carData : globalState.carsReducer,
-  carFeaturesData : globalState.carFeaturesReducer
-}
-}
-export default  connect(mapStateToProps,{addFeature,removeFeature,addFeatureBack,removeFeatureBack})(App);
-
-
-
-
-
+  return {
+    carData: globalState.carsReducer,
+    carFeaturesData: globalState.carFeaturesReducer,
+  };
+};
+export default connect(mapStateToProps, {
+  addFeature,
+  removeFeature,
+  addFeatureBack,
+  removeFeatureBack,
+})(App);
 
 // console.log("carFeaturesData Check",  sortObj("hey"))
 //     const sortObj = (recObj) => {
@@ -91,12 +80,9 @@ export default  connect(mapStateToProps,{addFeature,removeFeature,addFeatureBack
 //       }
 //       }
 
-
 // console.log("carFeaturesData Check",  sortObj("hey"))
 // function sortObj (recObj) {
 //   return{
 //     recObj
 //   }
 //   }
-
-
